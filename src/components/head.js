1cd0cad0a3acef8c0e5bf89f18f7edb0c0819ff6
hugo-dystech <hugor@dystech.com.au>
2021-01-14 16:13:21 +1100

    Improved Fav icon

--- src/components/head.js
+++ src/components/head.js
@@ -1,6 +1,9 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import Fav from "../images/favicon.png"
+import Fav32 from "../images/favicon-32x32.png"
+import Fav16 from "../images/favicon-16x16.png"
+import AppleTouch from "../images/apple-touch-icon.png"
+import FavTab from "../images/safari-pinned-tab.svg"
 
 
 class Head extends React.Component {
@@ -15,7 +18,12 @@ class Head extends React.Component {
     <meta name="description" content={this.props.description} />
     <meta name="keywords" content={this.props.keyword} />
     <meta name="author" content="Hugo Richard" />
-    <link rel="icon" href={Fav} type="image/gif" sizes="16x16"/>
+    <link rel="apple-touch-icon" sizes="180x180" href={AppleTouch} />
+    <link rel="icon" type="image/png" sizes="32x32" href={Fav32} />
+    <link rel="icon" type="image/png" sizes="16x16" href={Fav16} />
+    <link rel="mask-icon" href={FavTab} color="#5bbad5" />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="theme-color" content="#ffffff"/>
 
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
                 </Helmet>
