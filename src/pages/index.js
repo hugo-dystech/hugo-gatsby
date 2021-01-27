741e5b403be80ac53e58ce2f42cb21d2b4844fe1
hugo-dystech <hugor@dystech.com.au>
2021-01-28 09:41:28 +1100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const IndexPage = () => {
             <div className="col-12 col-md-6 m-auto pb-5 pb-md-0 pt-5 ps-5 pe-5 pt-md-0">
                 <h1>I help <u>people</u> make better <u>startups</u>.</h1>
                 <p> I live and breathe startups, and I have worked in 3 different countries in 2 continents over the last 6 years. I have good experience in marketing & growth with SaaS startups and I am a firm believer in profit for purpose.</p>
-                <p><Link className="fs-5 text-dark text-decoration-none plain-hover-black" to="mailto:zhugok@gmail.com"><strong>Let's talk <span role="img" aria-label="send email">&#128073;</span></strong></Link></p>
+                <p><a className="fs-5 text-dark text-decoration-none plain-hover-black" href="mailto:zhugok@gmail.com"><strong>Let's talk <span role="img" aria-label="send email">&#128073;</span></strong></a></p>
                 <Link to="https://www.linkedin.com/in/hugorichard/" target="blank"> <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
                     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
                   </svg></Link>
@@ -39,10 +39,10 @@ const IndexPage = () => {
     <nav id="innerNav" className="navbar shadow-sm navbar-expand-lg navbar-light bg-light sticky-lg-top pe-lg-5 ps-lg-5">
         <div className="container-fluid">
 
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#secondNav" aria-controls="secondNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
+            <div className="collapse navbar-collapse" id="secondNav">
 
                 <ul className="navbar-nav">
                     <li className="nav-item">
@@ -71,7 +71,7 @@ const IndexPage = () => {
                 </ul>
 
             </div>
-            <Link className="navbar-brand plain-hover-black" to="mailto:zhugok@gmail.com">Let's talk <span role="img" aria-label="send email">&#128073;</span></Link>
+            <a className="navbar-brand plain-hover-black" href="mailto:zhugok@gmail.com">Let's talk <span role="img" aria-label="send email">&#128073;</span></a>
 
         </div>
     </nav>
